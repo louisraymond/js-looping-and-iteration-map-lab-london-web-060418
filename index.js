df6687f9c376e14e4e8d70329e54f4b7bbfe141d
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-15 12:15:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // Code your solution in this file.
 
 function lowerCaseDrivers(array){
-  array.map( function(name){
-    return name.toLowerCase
+  return array.map( function(name){
+    name.toLowerCase
   })
+}
+
+function nameToAttributes (list) {
+  return list.map(function (driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };
+  });
+}
+
+
+function nameToAttributes(array){
+    return
 }
