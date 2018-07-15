af5ea6969dfc2456e150042930602ad2a695342e
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-15 12:19:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(array){
+  return array.map( function(name){
+   return  name.toLowerCase()
+  });
+}
+
+
+
+function nameToAttributes(array){
+    return array.map(function(name){
+      const First=name.split( ' ')[0];
+      const Last = name.split(' ')[1];
+      
+      return {firstName: First, lastName: Last}
+    })
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
+
